35b289a0f146ea88f7694b5563371ce135e54c4c
Ian Mungovan <57017900+ITM007@users.noreply.github.com>
2021-08-18 09:42:39 -0400

    feat: new Home Page
    adehomeV2 the new primary hoem page and mvoed old home to HomeV2. Made the Enter apst meeting button
    disa

--- src/frontend/src/views/HomeV2.vue
+++ src/frontend/src/views/HomeV2.vue
@@ -1,263 +1,267 @@
+d
 <template>
   <div>
     <Statistics :rows="rows" />
-
-    <div @keyup.esc="PastMeetingModal = false" class="flex justify-center">
-      <label
-        for="employeeNumber"
-        class="block text-md font-medium text-gray-700 justify-center mr-6"
-      >
-        Employees: &nbsp;
-        <input
-          type="text"
-          name="employeeNumber"
-          v-model="employeeNumber"
-          placeholder="0"
-          class="
-            mb-6
-            shadow-sm
-            focus:ring-bg-rrblue-400
-            focus:border-bg-rrblue-400
-            block
-            w-30
-            sm:text-sm
-            border-gray-300
-            rounded-md
-          "
-          :class="{ '': employeeNumber == 0 }"
-        />
-      </label>
-      <label for="title" class="block text-md font-medium text-gray-700 mr-6">
-        Meeting Title: &nbsp;
-        <input
-          type="text"
-          name="title"
-          v-model="title"
-          placeholder="Discuss Things..."
-          class="
-            mb-6
-            shadow-sm
-            focus:ring-bg-rrblue-400
-            focus:border-bg-rrblue-400
-            block
-            w-full
-            sm:text-sm
-            border-gray-300
-            rounded-md
-          "
-        />
-      </label>
-      <label
-        for="numSlides"
-        class="block text-md font-medium text-gray-700 mr-6"
-      >
-        Powerpoint Slides: &nbsp;
-        <input
-          type="text"
-          name="numSlides"
-          v-model="numSlides"
-          placeholder="Discuss Things..."
-          class="
-            mb-6
-            shadow-sm
-            focus:ring-bg-rrblue-400
-            focus:border-bg-rrblue-400
-            block
-            w-full
-            sm:text-sm
-            border-gray-300
-            rounded-md
-          "
-        />
-      </label>
-      <label
-        for="Meeting Group"
-        class="block text-md font-medium text-gray-700 mr-6"
-      >
-        Meeting Group: &nbsp;
-        <input
-          type="text"
-          name="meetingGroup"
-          v-model="meetingGroup"
-          placeholder="Group..."
-          class="
-            mb-6
-            shadow-sm
-            focus:ring-bg-rrblue-400
-            focus:border-bg-rrblue-400
-            block
-            w-full
-            sm:text-sm
-            border-gray-300
-            rounded-md
-          "
-        />
-      </label>
-      <!-- This is for the purpose dropdown -->
-      <!-- <DropDown /> -->
-
-      <!-- This creates our start and stop button for the counter -->
-    </div>
-    <div class="flex justify-center">
-      <label for="comment" class="text-md font-medium text-gray-700 mr-6 w-1/3">
-        Comment: &nbsp;
-        <input
-          type="text"
-          name="comment"
-          v-model="comment"
-          placeholder="What was discussed..."
-          class="
-            mb-6
-            shadow-sm
-            focus:ring-bg-rrblue-400
-            focus:border-bg-rrblue-400
-            block
-            w-full
-            h-16
-            sm:text-sm
-            border-gray-300
-            rounded-md
-          "
-        />
-      </label>
-    </div>
-    <!-- Outputs the cost calculation -->
-    <div class="flex justify-center mb-10 mt-6 text-lg">
-      <p>
-        <span>
-          Cost $
-          <span class="text-red-500 font-semibold">{{
-            costCalc.toFixed(2)
-          }}</span></span
-        ><br />
-        <span class="" v-show="seconds < 60 && seconds != 0"
-          >Seconds
-          <span class="text-red-500 w-14 font-semibold">{{
-            seconds.toFixed(0)
-          }}</span></span
-        >
-        <span class="" v-show="minutes >= 1"
-          >Minutes
-          <span class="text-red-500 font-semibold">{{ minutes }}</span></span
-        >
-      </p>
-    </div>
-
-    <div class="flex justify-center">
-      <button
-        v-if="employeeNumber != 0"
-        class="
-          relative
-          inline-flex
-          items-center
-          px-4
-          py-2
-          rounded-l-md
-          border border-gray-300
-          bg-rrblue-400
-          text-md
-          font-medium
-          text-white
-          hover:bg-rrblue-200
-        "
-        @click="play()"
-      >
-        Start
-      </button>
-      <button
-        v-if="employeeNumber != 0"
-        class="
-          -ml-px
-          relative
-          inline-flex
-          items-center
-          px-4
-          py-2
-          border border-gray-300
-          bg-red-500
-          text-md
-          font-medium
-          text-white
-          hover:bg-red-400
-        "
-        @click="pause()"
-      >
-        Stop
-      </button>
-      <button
-        class="
-          -ml-px
-          relative
-          inline-flex
-          items-center
-          px-4
-          py-2
-          rounded-r-md
-          border border-gray-300
-          bg-rrblue-400
-          text-md
-          font-medium
-          text-white
-          hover:bg-rrblue-200
-        "
-        @click="reset()"
-      >
-        Clear
-      </button>
-    </div>
-    <div class="flex justify-center mt-10">
-      <button
-        class="
-          mr-6
-          delay-100
-          duration-200
-          bg-rrblue-400
-          hover:bg-rrblue-200
-          text-white
-          font-semibold
-          py-3
-          px-4
-          rounded-md
-          text-lg
-          focus:outline-none
-        "
-        @click="PastMeetingModal = !PastMeetingModal"
-      >
-        <teleport to="#modals">
-          <PastMeetingForm
-            v-if="PastMeetingModal"
-            @close="hideModal()"
-            @updateTable="updateTable()"
+    <div @keyup.esc="pastMeetingModal = false" class="flex">
+      <div class="ml-8 w-screen">
+        <div class="flex justify-end mr-8">
+          <button
+            class="
+              delay-100
+              duration-200
+              bg-rrblue-400
+              hover:bg-rrblue-200
+              text-white
+              font-semibold
+              py-2
+              px-4
+              rounded-md
+              text-lg
+              focus:outline-none
+            "
+            @click="pastMeetingModal = !pastMeetingModal"
           >
-          </PastMeetingForm>
-        </teleport>
-        Enter Past Meeting
-      </button>
+            <teleport to="#modals">
+              <PastMeetingForm
+                v-if="pastMeetingModal"
+                @close="pastMeetingModal = false"
+                @updateTable="fetchTable()"
+              >
+              </PastMeetingForm>
+            </teleport>
+            Enter Past Meeting
+          </button>
+        </div>
+        <label
+          for="employeeNumber"
+          class="block text-md font-medium text-gray-700"
+        >
+          Employees: &nbsp;
+          <input
+            type="text"
+            name="employeeNumber"
+            v-model="employeeNumber"
+            placeholder="0"
+            class="
+              mb-6
+              shadow-sm
+              focus:ring-bg-rrblue-400
+              focus:border-bg-rrblue-400
+              block
+              w-30
+              sm:text-sm
+              border-gray-300
+              rounded-md
+            "
+            :class="{ '': employeeNumber == 0 }"
+          />
+        </label>
 
-      <button
-        class="
-          mr-2
-          delay-100
-          duration-200
-          bg-rrblue-400
-          hover:bg-rrblue-200
-          text-white
-          font-semibold
-          py-3
-          px-6
-          rounded-md
-          text-lg
-          focus:outline-none
-        "
-        @click.prevent="onSubmit"
-      >
-        Submit Meeting
-      </button>
+        <!-- This creates our start and stop button for the counter -->
+        <div class="flex-auto">
+          <button
+            v-if="employeeNumber != 0"
+            class="
+              delay-100
+              duration-200
+              bg-rrblue-400
+              hover:bg-rrblue-200
+              text-white
+              font-semibold
+              py-2
+              px-4
+              rounded-md
+              text-lg
+              focus:outline-none
+              mr-8
+            "
+            @click="timerEnabled = true"
+          >
+            Start
+          </button>
+          <button
+            v-if="employeeNumber != 0"
+            class="
+              delay-100
+              duration-200
+              bg-rrblue-400
+              hover:bg-rrblue-200
+              text-white
+              font-semibold
+              py-2
+              px-4
+              rounded-md
+              text-lg
+              focus:outline-none
+              mr-8
+            "
+            @click="timerEnabled = false"
+          >
+            Stop
+          </button>
+          <button
+            class="
+              delay-100
+              duration-200
+              bg-rrblue-400
+              hover:bg-rrblue-200
+              text-white
+              font-semibold
+              py-2
+              px-4
+              rounded-md
+              text-lg
+              focus:outline-none
+            "
+            @click="(costCalc = 0), (seconds = 0)"
+          >
+            Clear
+          </button>
+        </div>
+        <!-- Outputs the cost calculation -->
+        <div class="mt-4 text-md">
+          <p>
+            <span>
+              Cost $
+              <span class="text-rrpink-600 font-semibold">{{
+                costCalc.toFixed(2)
+              }}</span></span
+            ><br />
+            <span class="" v-show="seconds < 60 && seconds != 0"
+              >Seconds
+              <span class="text-rrpink-600 w-14 font-semibold">{{
+                seconds.toFixed(0)
+              }}</span></span
+            >
+            <span class="" v-show="minutes >= 1"
+              >Minutes
+              <span class="text-rrpink-600 font-semibold">{{
+                minutes
+              }}</span></span
+            >
+          </p>
+        </div>
+      </div>
     </div>
+    <!-- This is the Table rendered in the home page -->
+    <!-- <Lower
+      class="mt-4"
+      :cost="costCalc"
+      :employeeNumber="parseInt(employeeNumber)"
+      :minutes="minutes"
+      ref="fetchTablefromHome"
+    /> -->
+    <div class="mt-4">
+      <table class="ml-8">
+        <tbody>
+          <tr>
+            <label for="title" class="block text-md font-medium text-gray-700">
+              Meeting Title: &nbsp;
+              <input
+                type="text"
+                name="title"
+                v-model="title"
+                placeholder="Discuss Things..."
+                class="
+                  mb-6
+                  shadow-sm
+                  focus:ring-bg-rrblue-400
+                  focus:border-bg-rrblue-400
+                  block
+                  w-full
+                  sm:text-sm
+                  border-gray-300
+                  rounded-md
+                "
+              />
+            </label>
+          </tr>
+          <tr>
+            <label
+              for="comment"
+              class="block text-md font-medium text-gray-700"
+            >
+              Comment: &nbsp;
+              <input
+                type="text"
+                name="comment"
+                v-model="comment"
+                placeholder="What was discussed..."
+                class="
+                  mb-6
+                  shadow-sm
+                  focus:ring-bg-rrblue-400
+                  focus:border-bg-rrblue-400
+                  block
+                  w-full
+                  sm:text-sm
+                  border-gray-300
+                  rounded-md
+                "
+              />
+            </label>
+          </tr>
+          <tr>
+            <td class="text-md font-medium text-gray-700">
+              <p>Was PowerPoint Used?</p>
+            </td>
+            <td class="py-2">
+              <Toggle
+                @click="powerpoint = !powerpoint"
+                :toggleValue="powerpoint"
+              ></Toggle>
+            </td>
+          </tr>
+          <tr v-show="powerpoint">
+            <label
+              for="numSlides"
+              class="block text-md font-medium text-gray-700"
+            >
+              Number of Slides: &nbsp;
+              <input
+                type="text"
+                name="numSlides"
+                v-model="numSlides"
+                placeholder="Discuss Things..."
+                class="
+                  mb-6
+                  shadow-sm
+                  focus:ring-bg-rrblue-400
+                  focus:border-bg-rrblue-400
+                  block
+                  w-full
+                  sm:text-sm
+                  border-gray-300
+                  rounded-md
+                "
+              />
+            </label>
+          </tr>
+        </tbody>
+      </table>
 
-    <!-- <my-button v-on:click="handleClick">Test</my-button> -->
-
-    <Table class="mt-6" :rows="rows" @fetchTableAfterDelete="fetchTable()" />
+      <div class="mt-4 ml-8">
+        <button
+          class="
+            delay-100
+            duration-200
+            bg-rrblue-400
+            hover:bg-rrblue-200
+            text-white
+            font-semibold
+            py-2
+            px-4
+            rounded-md
+            text-lg
+            focus:outline-none
+          "
+          @click.prevent="onSubmit"
+        >
+          Submit
+        </button>
+      </div>
+      <Table class="mt-6" :rows="rows" @fetchTableAfterDelete="fetchTable" />
+    </div>
   </div>
 
   <!-- This is the charts that are rendered in the home page -->
@@ -267,59 +271,68 @@
 <script>
 import { ref, onMounted, computed, watch } from "vue";
 import axios from "axios";
+
 // import Lower from "../components/Lower.vue";
 import PastMeetingForm from "../components/PastMeetingForm.vue";
 import Statistics from "../components/Statistics.vue";
 import Toggle from "../components/Toggle.vue";
 import Table from "../components/Table.vue";
 import { nanoid } from "nanoid";
+
 export default {
   name: "Home",
+
   components: {
     PastMeetingForm,
     Statistics,
     Toggle,
     Table,
   },
+
   setup() {
     const HOURLYRATE = 135;
-    const meetingGroup = ref("");
+    const powerpoint = ref(false);
     const numSlides = ref(0);
     const rows = ref([]);
     const comment = ref("");
     const title = ref("");
     const timerEnabled = ref(false);
     const costCalc = ref(0);
-    const groupCost = ref(0);
     const seconds = ref(0);
     const pastMeetingModal = ref(false);
     const employeeNumber = ref(0);
+
     let minutes = computed(() => {
       return Math.floor(seconds.value / 60);
     });
+
     function resetForm() {
       employeeNumber.value = 0;
       title.value = "";
       comment.value = "";
+      powerpoint.value = false;
       numSlides.value = 0;
     }
+
     watch(timerEnabled, () => {
       if (timerEnabled) {
         costCalc.value += 0.000001;
       }
     });
+
     watch(
       costCalc,
       () => {
         if (costCalc.value > 0 && timerEnabled.value) {
           setTimeout(() => {
             costCalc.value = costCalc.value + HOURLYRATE / 60 / 60;
-            Math.floor((seconds.value += 1 / employeeNumber.value));
+            seconds.value = seconds.value + 1 / employeeNumber.value;
           }, 1000 / employeeNumber.value);
         }
       },
       { immediate: true }
     );
+
     // Fetch table data from the API
     function fetchTable() {
       axios
@@ -337,6 +350,7 @@ export default {
           console.log("Get Error: ", error);
         });
     }
+
     // Send data for a new row to the api
     function sendRow(rowVals) {
       axios
@@ -364,21 +378,22 @@ export default {
         date: Date.now(),
         employeeNumber: parseInt(employeeNumber.value),
         time: parseInt(minutes.value),
-        totalCost: parseFloat(costCalc.value.toFixed(2)),
-        meetingGroup: meetingGroup.value,
+        powerpoint: powerpoint.value,
         powerpointSlides: parseInt(numSlides.value),
+        totalCost: parseFloat(costCalc.value.toFixed(2)),
         comment: comment.value,
         title: title.value,
-        groupCost: groupCost.value,
       };
       // console.log("rowvals: ", rowVals);
       sendRow(rowVals);
     }
+
     onMounted(() => {
       fetchTable();
     });
+
     return {
-      meetingGroup,
+      powerpoint,
       numSlides,
       fetchTable,
       rows,
@@ -392,7 +407,6 @@ export default {
       seconds,
       pastMeetingModal,
       employeeNumber,
-      groupCost,
     };
   },
 };
