d7d2f83940e9e6911e6afe596136728c794ed696
Josh Haines <Josh@JoshHaines.com>
2021-09-02 15:03:24 -0400

    build(pre-commit): configure black in  pre-commit hook

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from starlette.testclient import TestClient
+
 # from mec.models import Meeting
 from mec.main import app
+
 # Creates a new connection
 @pytest.fixture(scope="module")
 def test_app():
