b10e3f05d6ae0c276866c1fb897a84f36a49f9bb
Ian Mungovan <ian.mungovan1@gmail.com>
2021-09-02 11:51:55 -0400

    Date Test

--- src/frontend/cypress/integration/Frontend.spec.js
+++ src/frontend/cypress/integration/Frontend.spec.js
@@ -1,4 +1,7 @@
+// var date = new Date();
 describe("MEC Frontend Tests", () => {
+  const dayjs = require("dayjs");
+  const todaysDate = dayjs().format("YYYY-MM-DD");
   it("Load Home Page", function () {
     cy.visit("localhost:3000");
   });
@@ -8,22 +11,25 @@ describe("MEC Frontend Tests", () => {
   // });
 
   // Inputs data into the data table then compares that data to the table output farther down
-  it("Inputing Data into table", function () {
-    cy.visit("localhost:3000");
-    cy.get("#employeeNumber").clear();
-    cy.get("#employeeNumber").type("5426");
-    cy.get("#meetingTitle").clear();
-    cy.get("#meetingTitle").type("Test Meeting Title");
-    cy.get("#numSlides").clear();
-    cy.get("#numSlides").type(9635);
-    cy.get("#meetingGroup").clear();
-    cy.get("#meetingGroup").type("Group Test abc123");
-    cy.get("#comment").clear();
-    cy.get("#comment").type("Test Comment");
-    cy.get("#start").click().wait(100);
-    cy.get("#stop").click();
-    cy.get("#submit").click();
-  });
+
+  // it("Inputing Data into table", function () {
+
+  //   cy.moment().format('DD','MM','YYYY')
+  //   cy.visit("localhost:3000");
+  //   cy.get("#employeeNumber").clear();
+  //   cy.get("#employeeNumber").type("5426");
+  //   cy.get("#meetingTitle").clear();
+  //   cy.get("#meetingTitle").type("Test Meeting Title");
+  //   cy.get("#numSlides").clear();
+  //   cy.get("#numSlides").type(9635);
+  //   cy.get("#meetingGroup").clear();
+  //   cy.get("#meetingGroup").type("Group Test abc123");
+  //   cy.get("#comment").clear();
+  //   cy.get("#comment").type("Test Comment");
+  //   cy.get("#start").click().wait(100);
+  //   cy.get("#stop").click();
+  //   cy.get("#submit").click();
+  // });
 
   // it("Verify Data in dataTable", function () {
   //   cy.get("#dataTable").contains("Test Meeting Title");
@@ -35,11 +41,12 @@ describe("MEC Frontend Tests", () => {
   // });
 
   it("Past Meeting Form", function () {
+    // cy.clock();
     cy.get("#employeeNumber").clear();
     cy.get("#pastMeeting").click();
     cy.get("#pastMeetingTitle").type("Past Meeting Title");
     cy.get("#pastMeetingGroup").type("Past Meeting Group");
-    cy.get("#pastMeetingDate").type("2021-01-22");
+    cy.get("#pastMeetingDate").type(todaysDate);
     cy.get("#pastMeetingComment").type("Past Meeting Comment");
     cy.get("#pastMeetingEmployeeNumber").clear().type("999");
     cy.get("#pastMeetingMinutes").clear().type("888");
@@ -58,21 +65,23 @@ describe("MEC Frontend Tests", () => {
   //   cy.get("#dataTable").contains("Past Meeting Comment");
   // });
 
-  it("Verifies data displayed in total statistics", function () {
-    cy.get("#totalClicked").contains("444");
-    cy.get("#totalTime").contains("888");
-    cy.get("#totalCost").contains("1996000");
-    cy.get("#averageCost").contains("99800");
-    cy.get("#totalSlides").contains("10412");
-  });
+  // it("Verifies data displayed in total statistics", function () {
+  //   cy.get("#totalButton").click();
+  //   cy.get("#totalClicked").contains("444");
+  //   cy.get("#totalTime").contains("888");
+  //   cy.get("#totalCost").contains("199600");
+  //   cy.get("#averageCost").contains("99800");
+  //   cy.get("#totalSlides").contains("10412");
+  // });
 
-  it("Verifies data displayed in last 30 days statistics", function () {
-    cy.get("#totalClicked").contains("0");
-    cy.get("#totalTime").contains("0");
-    cy.get("#totalCost").contains("1.");
-    cy.get("#averageCost").contains("1.");
-    cy.get("#totalSlides").contains("9635");
-  });
+  // it("Verifies data displayed in last 30 days statistics", function () {
+  //   cy.get("#daysButton").click();
+  //   cy.get("#totalClicked").contains("0");
+  //   cy.get("#totalTime").contains("0");
+  //   cy.get("#totalCost").contains("1.");
+  //   cy.get("#averageCost").contains("1.");
+  //   cy.get("#totalSlides").contains("9635");
+  // });
 
   // it("Deletes Past Meeting Data From Table", function () {
   //   cy.get("#deleteButton").click();
